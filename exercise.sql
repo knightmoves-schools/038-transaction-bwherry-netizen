67e30d81956380e20b16c42f213757cae8d6f878
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-05 12:05:09 -0500

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,14 @@
---Add your SQL here
+
+BEGIN TRANSACTION;
+
+INSERT INTO Stores VALUES (1, 'New York', 1050, 50000);
+
+INSERT INTO Employee VALUES (1050, 'John', 'Manager', 'Nashville');
+
+INSERT INTO Contact_Info VALUES (1050, 'john@email.com', '555-1234', '123 Main St', '90210');
+
+UPDATE Grocery
+SET NEXT_EXPIRE_DATE = DATE()
+WHERE PRODUCT_NAME IN ('Spinach', 'Avocado', 'Yogurt');
+
+COMMIT;
