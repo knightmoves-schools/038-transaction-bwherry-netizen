d21c011daaefba0ad80c074af12dedcabeb60ad4
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-05 12:07:17 -0500

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,11 +1,28 @@
-
 BEGIN TRANSACTION;
 
-INSERT INTO Stores VALUES (1, 'New York', 1050, 50000);
+INSERT INTO Stores
+VALUES (1, 'New York', 1050, 50000);
 
-INSERT INTO Employee VALUES (1050, 'John', 'Manager', 'Nashville');
+INSERT INTO Employee
+VALUES (
+  1050,
+  'John',
+  'Smith',
+  60000,
+  'Nashville',
+  DATE(),
+  'College'
+);
 
-INSERT INTO Contact_Info VALUES (1050, 'john@email.com', '555-1234', '123 Main St', '90210');
+INSERT INTO Contact_Info
+VALUES (
+  1050,
+  'john@email.com',
+  5551234,
+  '123 Main St',
+  90210,
+  'Jane Smith'
+);
 
 UPDATE Grocery
 SET NEXT_EXPIRE_DATE = DATE()
